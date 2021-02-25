6fcc9d412e108bf5cbadf45199311ea2c0fe36e7
Matthew-Ruvalcaba <mate@pop-os.localdomain>
2021-02-25 09:55:16 -0800

    Primer commit

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,11 @@
+class SayController < ApplicationController
+# En el controlador definiremos una instancia para que despues View la utilice para mostrar un resultado al usuario
+  def hello
+    @time = Time.now
+  end
+# Podemos definir las funciones del controlador en una vista y funcionara de la misma manera.
+# Pero no sera una buena pratica si queremos realizar la misma funcion varias veces,
+# es por eso que en controlador definimos una forma general y en la vista manipulamos.
+  def goodbye
+  end
+end
