0b7245197ea884cb02b3b68c2e425380170c8654
Matthew-Ruvalcaba <mate@pop-os.localdomain>
2021-02-25 10:49:17 -0800

    primer_migracion

--- db/migrate/20210225181839_create_products.rb
+++ db/migrate/20210225181839_create_products.rb
@@ -1,10 +1,14 @@
+# Aqui manipulamos los datos dentro de nuestra db
+# Para hacer cambios dentro de nuestra base de datos, debemos aplicar:
+# bin/rails db:migrate
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price
+      # agregamos dos digitos al timestampo y decimales.
+      t.decimal :price, precision: 8, scale: 2 
 
       t.timestamps
     end
