c6959fbf41f4851525714700976cc09981b0a172
Matthew Ruvalcaba <mattr@enjoykitchen.mx>
2021-02-26 15:49:16 -0800

    inicio del carro

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,5 +6,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4 #checamos que existan 4 elementos en la barra de navegacion
+    assert_select 'main ul.catalog li', 3 # revisamos elementos de nombre del main
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/ # revisamos atributos de clase (precio del producto) 
   end
 end
