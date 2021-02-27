44bb50008bf7d72f7439615a96ae1b4369fd4287
Matthew Ruvalcaba <mattr@enjoykitchen.mx>
2021-02-27 15:09:37 -0800

    archivos de carro

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :carts
+  resources :line_items
   root 'store#index', as: 'store_index'
   resources :products
   get 'say/hello'
